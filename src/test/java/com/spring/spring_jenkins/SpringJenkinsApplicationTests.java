edc7ea045bd1e9ce916c441d9303c41249565053
Shahar-bin-Akbar <shaharali141@gmail.com>
2026-01-03 15:28:48 +0530

    handling errors

--- src/test/java/com/spring/spring_jenkins/SpringJenkinsApplicationTests.java
+++ src/test/java/com/spring/spring_jenkins/SpringJenkinsApplicationTests.java
@@ -11,7 +11,7 @@ class SpringJenkinsApplicationTests {
 	private static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
 
 	@Test
-	void contextLoads() {
+	 contextLoads() {
 	   logger.info("Testcase Executed");	
 	}
 
